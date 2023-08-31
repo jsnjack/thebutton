fdf92defde7ec4fac5124031312dc9738ef4b655
Yauhen Shulitski <jsnjack@gmail.com>
2023-08-31 22:57:49 +0200

    :m: Look for active Surfly session in all windows, not just current

--- src/background.js
+++ src/background.js
@@ -51,7 +51,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 // Get the ID of the tab which runs Surfly session
 async function getSurflyTabID() {
     const tabs = await chrome.tabs.query({
-        currentWindow: true,
+        currentWindow: false,
         url: [
             `https://${surflyServer}/*`,  // Regular Surfly session or Space
             `https://*.${surflyServer}/*`,  // Space session with custom subdomain
